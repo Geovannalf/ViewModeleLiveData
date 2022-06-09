babef09d36044437321676873da8b58261cd10cd
Geovannalf <geovannalima0205@gmail.com>
2022-06-09 19:22:17 -0300

    Exercicios utilizando ViewModel e LiveData

---
+++ app/src/main/java/com/example/viewmodel_livedata/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.viewmodel_livedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodel_livedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mainViewModel = ViewModelProvider(this)
+            .get(MainViewModel::class.java)
+
+        mainViewModel.cont.observe(this){
+            binding.textResultado.text = it.toString()
+        }
+
+        binding.buttonClique.setOnClickListener{
+            mainViewModel.addNum()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/viewmodel_livedata/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.viewmodel_livedata
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel (){
+    var cont = MutableLiveData<Int>(0)
+
+    fun addNum(){
+        cont.value= cont.value?.plus(1)
+    }
+}
